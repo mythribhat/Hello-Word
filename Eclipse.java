6f0041898ecad31cbc6a4c7679ff0c97f7b37da5
mythri <mythri.r@gmail.com>
2018-01-29 18:16:50 +0530

    java program

---
+++ Eclipse.java
@@ -0,0 +1,15 @@
+
+public class Eclipse{
+	public static void main(String[] args) {
+		int i ;
+		// TODO Auto-generated method stub
+		for(i=0;i<10;i++)
+		{
+		System.out.println("Mythri");
+		System.out.println("super");
+		
+
+	}
+
+}
+}
